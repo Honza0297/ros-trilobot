b4ebd1df8218857df3bcd9ab713cb1fc002e66c5
Jan Beran <jaberan@redhat.com>
2022-04-26 19:04:37 +0200

    Introduce arduino rosserial starter - rpi site

--- scripts/start_arduino_rosserial.py
+++ scripts/start_arduino_rosserial.py
@@ -2,19 +2,30 @@
 
 import rospy
 from trilobot.msg import *
-from std_msgs.msg import Empty, String, Int32
-from trilobot.msg import Sonar_data
+from trilobot.msg import Battery_state
 from topics import *
+
 class Starter():
-   def __init__(self) 
-        
+   def __init__(self):
+   		self.pub = rospy.Publisher(topic_rosserial_start, Empty, queue_size=10)
+        self.control_sub = rospy.Subscriber(topic_battery_raw, Battery_state, self.callback)
+        self.arduino_up = False
+
+    def callback(self, msg):
+    	self.arduino_up = True
+
+    def spin(self):
+    	rate = rospy.Rate(2):
+    	while not rospy.is_shutdwn():
+    		if self.arduino_up:
+    			rospy.signal_shutdown(None)
+    		self.pub.publish(Empty())
+    		rate.sleep()
+
 if __name__ == "__main__":
-    rospy.init_node('sonars_bridge')
-    pub = rospy.Publisher(topic_rosserial_start, Empty, queue_size=10)
-    sub = rospy.Subscriber(topic_trilobot_sonar_data, Sonar_data, callback)
-    msg = Empty()
-    pub.publish(msg)
-    rospy.spin() 
+    rospy.init_node('rosserial_arduino_starter')
+    starter = Starter()
+    starter.spin() 
 
     
 
